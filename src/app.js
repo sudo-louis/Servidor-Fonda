a3f41cf6c1fd191d574edfc607efb8d9e5d090c2
sudo-louis <luisito.da.123@gmail.com>
2025-03-28 14:13:33 -0600

    no vercel

--- src/app.js
+++ src/app.js
@@ -2,25 +2,19 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
-const connectDB = require("./config/db");
-
-// Conectar a MongoDB
-connectDB();
 
 const app = express();
 
-// Configurar CORS
+// app.use(cors());
+
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: 'http://localhost:5173',
     credentials: true
 }));
 
 app.use(express.json());
-
-// Servir archivos estáticos
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Importar rutas
 const adminAuthRoutes = require("./routes/adminAuthRoutes");
 const adminProtectedRoutes = require("./routes/adminProtectedRoutes");
 const productRoutes = require("./routes/productRoutes");
@@ -29,23 +23,22 @@ const categoryRoutes = require("./routes/categoryRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const clientAuthRoutes = require("./routes/clientAuthRoutes");
 
-// Definir rutas
-app.get('/', (req, res) => {
-    res.send('🚀 Servidor Express funcionando en Vercel!');
-});
-
+app.get('/', (req,res)=>{
+    res.send('Hola mundo')
+})
 app.use("/api/admin/auth", adminAuthRoutes);
 app.use("/api/admin", adminProtectedRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/providers", providerRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/orders", orderRoutes);
-app.use("/api/clientes", clientAuthRoutes);
+app.use('/api/clientes', clientAuthRoutes);
 
-// Ruta para obtener la configuración de PayPal
+
+app.use("/uploads", express.static(path.join(__dirname, "src/uploads")));
+
 app.get("/api/config/paypal", (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
-// Exportar la app para Vercel
 module.exports = app;
